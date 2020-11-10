b0108f4ebf1bce137dede0227775511cfea455bc
Vinicius <vinisilva45@live.com>
2020-11-10 15:36:12 -0300

    Criando senhas random.

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,13 @@
+export default{
+    async store(req, res){
+        const{ name, email } = req.body;
+
+        const user = {
+            name,
+            email,
+            password: passwordGenerator(15, false),
+        };
+
+        return res.json(user);
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import 'dotenv/config'; 
 import express from 'express';
 
+import UserController from '../src/app/controllers/UserController';
+
+
 const app = express();
 
 app.use(express.json());
+
+app.post('../users', UserController.store);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on the ${process.env.PORT}`)
